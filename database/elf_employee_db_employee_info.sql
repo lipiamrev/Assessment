193933f14f65097955901b8bb2f4a2b45e8269f8
lipiamrev <31662040+lipiamrev@users.noreply.github.com>
2019-12-09 16:06:52 +0530

    Add files via upload

---
+++ database/elf_employee_db_employee_info.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: elf_employee_db
+-- ------------------------------------------------------
+-- Server version	5.7.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee_info`
+--
+
+DROP TABLE IF EXISTS `employee_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employee_info` (
+  `emp_id` int(11) NOT NULL,
+  `emp_name` varchar(5) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `salary` double DEFAULT NULL,
+  `designation` varchar(20) DEFAULT NULL,
+  `mobile` bigint(20) DEFAULT NULL,
+  `password` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`emp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee_info`
+--
+
+LOCK TABLES `employee_info` WRITE;
+/*!40000 ALTER TABLE `employee_info` DISABLE KEYS */;
+INSERT INTO `employee_info` VALUES (1,'lipi',23,20000,'get',415263987,'qwerty'),(3,'Tavi',89,5000,'analyst',8748595659,'qwerty'),(4,'Lavi',42,5500,'analyst',8722595659,'qwerty'),(8,'sam',85,456,'sd',415263,'qwerty'),(678,'erkg',23,344556,'ed',5435156,'qwerty'),(2234,'gnj',453,234657,'hg',43235,'qwerty');
+/*!40000 ALTER TABLE `employee_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-09 16:04:55
